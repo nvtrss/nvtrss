ea8121234117318fd693aae2a67b17d07ec06feb
Joseph Price <pricechild@gmail.com>
2013-08-24 19:54:33 +0000

    Move individual feed update to own function.

--- update.py
+++ update.py
@@ -83,75 +83,77 @@ def updatefavicon(feed_url, feed_id):
               has_icon=has_icon,
               vars={'feed_id': feed_id})
 
+def updatefeed(feed):
+    logging.info("Processing %s" % feed.url)
+    result = feedparser.parse(feed.url, etag=feed.etag, modified=feed.last_modified)
+    if result.status == 304:
+        update_lastupdate(feed.feed_id)
+        logging.info("304 received, skipping.")
+        return False
+    db.update('feeds',
+              where="feed_id=$feed_id",
+              feed_title=result.feed.get('title', feed.url),
+              etag=result.get('etag', None),
+              last_modified=result.get('modified', None),
+              vars={'feed_id': feed.feed_id})
+    for entry in result.entries:
+        published = entry.get('published_parsed', None)
+        if published:
+            published = datetime.fromtimestamp(mktime(published))
+        updated = entry.get('updated_parsed', None)
+        if updated:
+            updated = datetime.fromtimestamp(mktime(updated))
+        content = None
+        if entry.description:
+            content = entry.description
+        try:
+            for c in entry.content:
+                if content:
+                    content+=c.value
+                else:
+                    content=c.value
+        except AttributeError:
+            #Not an atom feed
+            pass
+        guid = entry.get('id', entry.title)
+        try:
+            item = db.select('items',
+                             where="feed_id=$feed_id AND guid=$guid",
+                             vars={'feed_id': feed.feed_id, 'guid': guid})[0]
+            item_id = item.item_id
+            logging.debug("%s already exists." % guid)
+            if not item.updated or updated > item.updated:
+                logging.info("%s updated." % guid)
+                db.update('items',
+                          where="guid=$guid",
+                          title=entry.title,
+                          description=entry.description,
+                          link=entry.link,
+                          published=published,
+                          updated=updated,
+                          content=content,
+                          vars={'guid': guid})
+        except IndexError:
+            logging.info("%s new!" % guid)
+            item_id = db.insert('items',
+                                feed_id=feed.feed_id,
+                                title=entry.title,
+                                description=entry.description,
+                                link=entry.link,
+                                published=published,
+                                updated=updated,
+                                content=content,
+                                guid=guid)
+    update_lastupdate(feed.feed_id)
+    if not feed.icon_updated or feed.icon_updated > (datetime.utcnow() - timedelta(days=7)):
+        updatefavicon(feed.url, feed.feed_id)
+    logging.info("Finished.")
 
 def main(argv=None):
     if argv is None:
         argv = sys.argv
     for feed in feedstoprocess():
-        logging.info("Processing %s" % feed.url)
-        result = feedparser.parse(feed.url, etag=feed.etag, modified=feed.last_modified)
-        if result.status == 304:
-            update_lastupdate(feed.feed_id)
-            logging.info("304 received, skipping.")
-            continue
-        db.update('feeds',
-                  where="feed_id=$feed_id",
-                  feed_title=result.feed.get('title', feed.url),
-                  etag=result.get('etag', None),
-                  last_modified=result.get('modified', None),
-                  vars={'feed_id': feed.feed_id})
-        for entry in result.entries:
-            published = entry.get('published_parsed', None)
-            if published:
-                published = datetime.fromtimestamp(mktime(published))
-            updated = entry.get('updated_parsed', None)
-            if updated:
-                updated = datetime.fromtimestamp(mktime(updated))
-            content = None
-            if entry.description:
-                content = entry.description
-            try:
-                for c in entry.content:
-                    if content:
-                        content+=c.value
-                    else:
-                        content=c.value
-            except AttributeError:
-                #Not an atom feed
-                pass
-            guid = entry.get('id', entry.title)
-            try:
-                item = db.select('items',
-                                 where="feed_id=$feed_id AND guid=$guid",
-                                 vars={'feed_id': feed.feed_id, 'guid': guid})[0]
-                item_id = item.item_id
-                logging.debug("%s already exists." % guid)
-                if not item.updated or updated > item.updated:
-                    logging.info("%s updated." % guid)
-                    db.update('items',
-                              where="guid=$guid",
-                              title=entry.title,
-                              description=entry.description,
-                              link=entry.link,
-                              published=published,
-                              updated=updated,
-                              content=content,
-                              vars={'guid': guid})
-            except IndexError:
-                logging.info("%s new!" % guid)
-                item_id = db.insert('items',
-                                    feed_id=feed.feed_id,
-                                    title=entry.title,
-                                    description=entry.description,
-                                    link=entry.link,
-                                    published=published,
-                                    updated=updated,
-                                    content=content,
-                                    guid=guid)
-        update_lastupdate(feed.feed_id)
-        if not feed.icon_updated or feed.icon_updated > (datetime.utcnow() - timedelta(days=7)):
-            updatefavicon(feed.url, feed.feed_id)
-        logging.info("Finished.")
+        updatefeed(feed)
 
 if __name__ == "__main__":
     sys.exit(main())
