cbec93bd9920188b789459498a721336088509c5
Joseph Price <pricechild@gmail.com>
2013-08-24 19:35:04 +0000

    favicon nolonger 'TODO'..

--- server.py
+++ server.py
@@ -233,7 +233,6 @@ def getFeeds(sid, cat_id=None, offset=None, limit=None, **args):
                           'title': feed.feed_title,
                           'id': feed.feed_id,
                           'unread': unread,
-                          #TODO: favicon support
                           'has_icon': feed.has_icon,
                           'cat_id': feed.cat_id,
                           'last_updated': feed.lastupdate.strftime("%s"),
